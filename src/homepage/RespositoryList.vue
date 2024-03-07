d35b1a6ef8b9c00fe641c37e5273a758c7eadb5f
ianwonghanli <ian.wong.han.li@gmail.com>
2024-03-07 21:44:50 +0800

    Created a new Card div in Respository list with table. Table will be modified to be multi-page

--- src/homepage/RespositoryList.vue
+++ src/homepage/RespositoryList.vue
@@ -1,14 +1,38 @@
 <template>
     <div class="assets-container">
-        <div class="assets" @click="openModal('Cash')"> <strong> Cash </strong> </div>
+        <!-- <div class="assets" @click="openModal('Cash')"> <strong> Cash </strong> </div>
         <div class="assets" @click="openModal('Stocks')"> <strong> Stocks </strong> </div>
         <div class="assets" @click="openModal('Bonds')"> <strong> Bonds </strong> </div>
-        <div class="assets" @click="openModal('CPF')"> <strong> CPF </strong> </div>
-        <p> {{ filings }}</p>
-        <CashModal v-model:isVisible="modals.cash" />
+        <div class="assets" @click="openModal('CPF')"> <strong> CPF </strong> </div> -->
+        <div class="card" :class="{ expanded: isExpanded }" @click="isExpanded = !isExpanded">
+            <div class="card-header">
+                <h2> Card Title </h2>
+            </div>
+        </div>
+
+        <div class="cardExpand" v-show="isExpanded">
+            <p> This is some content that will show up when the card is expanded. Click the card to toggle. </p>
+            <table class = "stock-repository-table">
+                <thead>
+                    <tr>
+                        <th v-for="header in ['test', 'hi', 'there']" > {{ header }} </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in [1,2,4]">
+                        <td v-for="cell in ['test', 'hi', 'there']">
+                            {{ cell }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <p> {{ filings }} </p>
+        <!-- <CashModal v-model:isVisible="modals.cash" />
         <StocksModal v-model:isVisible="modals.stocks" />
         <BondsModal v-model:isVisible="modals.bonds" />
-        <CPFModal v-model:isVisible="modals.cpf" />
+        <CPFModal v-model:isVisible="modals.cpf" /> -->
     </div>
 </template>
 
@@ -30,6 +54,7 @@ export default {
     },
     data() {
         return {
+            isExpanded: false,
             modals: {
                 cash: false,
                 stocks: false,
@@ -37,7 +62,7 @@ export default {
                 cpf: false
             },
             apiKey: '1cd758d99b72ba099d116f339f8bc0170ca1bfc1665c25189dddcbc4ca107748',
-            filings: null
+            filings: null,
         };
     },
     methods: {
