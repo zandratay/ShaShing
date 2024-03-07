5c0f2f47e3887aa81bd0c61334cd1913c3794a3b
ianwonghanli <ian.wong.han.li@gmail.com>
2024-03-07 10:36:40 +0800

    Changed name on header

--- src/homepage/Header.vue
+++ src/homepage/Header.vue
@@ -2,7 +2,7 @@
     <img id="header-img" src="https://wallpapers.com/images/featured/widescreen-3ao0esn9qknhdudj.jpg" alt="Duck UI">
 
     <div class="welcome-info">
-        <h2> Welcome, John! </h2>
+        <h2> Welcome, Ian! </h2>
         <h3> Current Portfolio </h3>
         <p> Updated at 22 March, 5pm </p>
     </div>
