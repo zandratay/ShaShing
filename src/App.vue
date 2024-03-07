5a52499ef13b40ac2992009200e7d19587fb70a9
ianwonghanli <ian.wong.han.li@gmail.com>
2024-03-07 12:20:54 +0800

    added a test stockRespository page and prelimanary switch between home and stock respository page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
 
   <div class="navbar">
-    <NavBar />
+    <NavBar @page_at="changePage($event)"/>
   </div>
   
   <div class="content">
     <Header />
-    <OverviewCharts />
+    <component :is="currentComponent"></component>
+    <!-- <OverviewCharts /> -->
     <AssetClass />
   </div>
   
@@ -15,15 +16,32 @@
 <script>
   import Header from  './homepage/Header.vue';
   import OverviewCharts from  './homepage/OverviewCharts.vue';
+  import StockRespository from  './homepage/StockRespository.vue';
   import AssetClass from  './homepage/AssetClass.vue';
   import NavBar from  './homepage/NavBar.vue';
 
   export default {
+    data(){
+      return {
+        currentComponent: OverviewCharts
+      }
+    },
     components: {
       Header, 
       OverviewCharts,
       AssetClass,
       NavBar
+    },
+    methods:{
+      changePage(page) {
+        console.log(page);
+        if (page === 'A') {
+          this.currentComponent = OverviewCharts;
+          console.log('A')
+        } else {
+          this.currentComponent = StockRespository;
+        }
+      }
     }
   }
 </script>
---
+++ src/homepage/StockRespository.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1> Stock Respository page </h1>
+    <div class="graphs-container">
+        <div class="charts">
+            <h4> Total Profit </h4><br><br>
+            <p> $1000 </p>
+        </div>
+
+        <div class="charts">
+            <h4> Your Portfolio </h4> <br><br>
+            <p> Insert line graph here</p>
+        </div>
+
+        <div class="charts">
+            <h4> Asset Composition </h4><br><br>
+            <p> Insert pie chart here</p>
+        </div>
+    </div>
+</template>
+
