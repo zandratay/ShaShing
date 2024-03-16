6ef713e49efbb32eec22f16d431d30fbdecc937f
Lao Kyle Daniel Kin <kyledaniel.lao@gmail.com>
2024-03-16 14:00:31 +0800

    first commit
    edited the graphs

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,27 @@
   <div class="navbar">
     <NavBar />
   </div>
-  
+
   <div class="content">
     <Header />
     <OverviewCharts />
     <AssetClass />
   </div>
-  
+
 </template>
 
 <script>
-  import Header from  './homepage/Header.vue';
-  import OverviewCharts from  './homepage/OverviewCharts.vue';
-  import AssetClass from  './homepage/AssetClass.vue';
-  import NavBar from  './homepage/NavBar.vue';
+import Header from './homepage/Header.vue';
+import OverviewCharts from './homepage/OverviewCharts.vue';
+import AssetClass from './homepage/AssetClass.vue';
+import NavBar from './homepage/NavBar.vue';
 
-  export default {
-    components: {
-      Header, 
-      OverviewCharts,
-      AssetClass,
-      NavBar
-    }
+export default {
+  components: {
+    Header,
+    OverviewCharts,
+    AssetClass,
+    NavBar,
   }
+}
 </script>
-
-
