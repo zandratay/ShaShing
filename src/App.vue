92be7f0860c8366feac927a25efa706ad0479d7c
ianwonghanli <ian.wong.han.li@gmail.com>
2024-03-16 15:17:33 +0800

    minor edits

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="navbar">
-    <NavBar @page_at="changePage($event)"/>
+    <NavBar/>
   </div>
   
   <div class="content">
@@ -17,25 +17,15 @@
   import OverviewCharts from  './homepage/OverviewCharts.vue';
   import StockRespository from  './homepage/StockRespository.vue';
   import AssetClass from  './homepage/AssetClass.vue';
-  import RespositoryList from  './homepage/RespositoryList.vue';
   import NavBar from  './homepage/NavBar.vue';
 
   export default {
-    data(){
-      return {
-        currentMainView: OverviewCharts,
-        currentList : AssetClass
-      }
-    },
     components: {
       Header, 
       OverviewCharts,
       AssetClass,
       RespositoryList,
       NavBar
-    },
-    methods:{
-      
     }
   }
 </script>
